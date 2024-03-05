da608d0759486c6912cb65f024ee32e7e18bad7a
prakashdeveloper11 <153601966+prakashdeveloper11@users.noreply.github.com>
2024-03-05 20:51:30 +0530

    Add files via upload

---
+++ loan/src/main/java/com/loan/form/CibilForm.java
@@ -0,0 +1,31 @@
+package com.loan.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import com.loan.dto.CibilDTO;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CibilForm extends BaseForm{
+	
+	@NotEmpty(message = "This is required")
+	
+	private String pancard;
+	
+	
+	public CibilDTO getDTO() {
+		CibilDTO bean = new CibilDTO();
+	     bean.setPancard(pancard);
+		return bean;
+	}
+
+	public void populate(CibilDTO bean) {
+		pancard = bean.getPancard();
+	}
+
+}
